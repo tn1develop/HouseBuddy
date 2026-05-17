9df13e04015bdb98e5f1078c08dacb11ca0271ed
Tommaso Natta <t.natta@reply.it>
2026-05-17 23:50:13 +0200

    Android app commit

--- androidApp/src/main/kotlin/com/example/housebuddy/MainActivity.kt
+++ androidApp/src/main/kotlin/com/example/housebuddy/MainActivity.kt
@@ -5,21 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.housebuddy.presentation.mvi.HousePriceViewModel
+import com.example.housebuddy.ui.screen.HousePriceScreen
+import com.example.housebuddy.ui.theme.HousePricesCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
-            App()
+            HousePricesCalculatorTheme {
+                val viewModel = remember { HousePriceViewModel() }
+                HousePriceScreen(
+                    state = viewModel.state,
+                    result = viewModel.result,
+                    onIntent = viewModel::handleEvent
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun GreetingPreview() {
+    HousePricesCalculatorTheme {
+        HousePriceScreen()
+    }
 }
